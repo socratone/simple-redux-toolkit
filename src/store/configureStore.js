3bf38a8c1b0b7ce86948f14aef696e6c24b5d0a3
socratone <gim2origin@gmail.com>
2020-12-03 02:56:43 +0900

    @reduxjs/toolkit을 썼을 때의 장점 주석

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -5,5 +5,9 @@ export default function () {
   return configureStore({ reducer });
 }
 
-// redux dev tools 설정을 자동으로 해준다.
-// dispatch asynchronous actions
+/**
+ * @reduxjs/toolkit을 썼을 때의 장점
+ * 
+ * 1. redux dev tools 설정을 자동으로 해준다.
+ * 2. dispatch asynchronous actions
+ */
